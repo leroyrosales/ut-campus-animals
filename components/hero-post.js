a22e21aa5acd25efdd7dd665225ce19543046afb
Leroy Rosales <leroyrosales@gmail.com>
2021-04-07 06:35:54 -0500

    Adds priority attribute to above fold images

--- components/hero-post.js
+++ components/hero-post.js
@@ -18,6 +18,7 @@ export default function HeroPost({
           slug={slug}
           height={600}
           width={900}
+          priority
         />
       </div>
       <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
