86b00f17b44d0786845687baaddcb59eaaf4f97b
Leroy Rosales <leroyrosales@gmail.com>
2021-04-07 09:52:09 -0500

    Swaps out image for cover image

--- components/cover-image.js
+++ components/cover-image.js
@@ -1,20 +1,17 @@
 import cn from 'classnames'
 import Link from 'next/link'
-import Image from 'next/image'
 
 export default function CoverImage({ title, src, slug, height, width }) {
   const image = (
-    <Image
+    <img
       src={src}
       alt={`Cover Image for ${title}`}
-      className={cn('shadow-sm', {
+      className={cn('w-full shadow-sm', {
         'hover:shadow-md transition-shadow duration-200': slug,
       })}
       layout="responsive"
       width={width}
       height={height}
-      quality={50}
-      priority={true}
     />
   )
   return (
