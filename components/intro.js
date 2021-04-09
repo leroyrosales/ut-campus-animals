fd947024b1a265b218afa7de5659302bcd8ceb69
Leroy Rosales <leroyrosales@gmail.com>
2021-04-09 12:49:59 -0500

    Adds tailwind to intro.js

--- components/intro.js
+++ components/intro.js
@@ -5,7 +5,9 @@ export default function Intro() {
     <>
       <h4 className="text-center md:text-right text-lg mb-5">A statically generated blog example using{' '}<a
         href="https://nextjs.org/"
-        className="underline hover:text-success duration-200 transition-colors">Next.js</a>{' '}with Markdown.</h4>
+        className="underline hover:text-ut-burntorange duration-200 transition-colors">Next.js</a>, <a
+        href="https://tailwindcss.com/"
+        className="underline hover:text-ut-burntorange duration-200 transition-colors">TailwindCSS</a>, and Markdown.</h4>
     </>
   )
 }
